f9c3623fae6ecbd2d9fa4ace1b2c86a9ef9fac5d
Beata <beata@jasinski.com>
2024-08-31 16:48:12 -0700

    chanel

--- main.go
+++ main.go
@@ -5,18 +5,26 @@ import (
 	"time"
 )
 
-func greet(phrase string) {
+func greet(phrase string, daneChan chan bool) {
 	fmt.Println("Hello!", phrase)
+	daneChan <- true
 }
 
-func slowGreet(phrase string) {
+func slowGreet(phrase string, daneChan chan bool) {
 	time.Sleep(3 * time.Second) // simulate a slow, long-taking task
 	fmt.Println("Hello!", phrase)
+	daneChan <- true
 }
 
 func main() {
-	go greet("Nice to meet you!")
-	go greet("How are you?")
-	go slowGreet("How ... are ... you ...?")
-	go greet("I hope you're liking the course!")
+	dane := make(chan bool)
+
+	go greet("Nice to meet you!", dane)
+	go greet("How are you?", dane)
+	go slowGreet("How ... are ... you ...?", dane)
+	go greet("I hope you're liking the course!", dane)
+	<-dane
+	<-dane
+	<-dane
+	<-dane
 }
